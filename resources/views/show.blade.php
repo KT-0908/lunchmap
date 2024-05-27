b8210d5ff6583cb6b5b2474917c5a304d3dd31b4
KT_0908 <lie1truth@gmail.com>
2024-05-27 18:06:18 +0900

    delete

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,5 +10,14 @@
 <div>
     <a href={{ route('shop.list')}}>一覧に戻る</a>
     | <a href={{ route('shop.edit', ['id' => $shop->id]) }}>編集</a>
+    <p></p>
+    <form method="POST" action="{{ route('shop.destroy', ['id' => $shop->id]) }}" onsubmit="return confirm('本当に削除しますか？')">
+    @csrf
+    @method('DELETE')
+
+    <button type="submit">削除</button>
+</form>
+
+
 </div>
 @endsection
