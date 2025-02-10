94034379c7ad5c381bee43e24726678c699c1a4c
KT_0908 <lie1truth@gmail.com>
2025-02-10 16:39:48 +0900

    User-Management

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -24,7 +30,10 @@ class ShopController extends Controller
     {
         // pluckは必要なカラムのデータだけ取り出せるメソッド
         // pluckは第二引数を取ることができ、pluck(valueにしたい値, keyにしたい値)という仕組みになっています
+        // Category モデルから全データを取得し、pluckで 'name' を value に、'id' を key にする
         $categories = Category::all()->pluck('name', 'id');
+
+        // new ビューに categories を渡す
         return view('new', ['categories' => $categories]);
     }
 
@@ -35,10 +44,18 @@ class ShopController extends Controller
     public function store(Request $request)
     {
         $shop = new Shop;
+        // ↓ログインしているユーザー情報を取得
+        $user = \Auth::user();
+
+        // フォームから送信された name, address, category_id を取得し、$shop にセット。
         $shop->name = request('name');
         $shop->address = request('address');
         $shop->category_id = request('category_id');
+        // ↓ログインしているユーザーのidを保存
+        $shop->user_id = $user->id;
         $shop->save();
+
+        // ['id' => $shop->id] で新しく作成した shop の ID を渡す。
         return redirect()->route('shop.detail', ['id' => $shop->id]);
     }
 
@@ -48,7 +65,14 @@ class ShopController extends Controller
     public function show($id)
     {
         $shop = Shop::find($id);
-        return view('show', ['shop' => $shop]);
+        $user = \Auth::user();
+        if ($user) {
+            $login_user_id = $user->id;
+        } else {
+            $login_user_id = "";
+        }
+        
+        return view('show', ['shop' => $shop, 'login_user_id' => $login_user_id]);
     }
 
     /**
@@ -56,8 +80,12 @@ class ShopController extends Controller
      */
     public function edit($id)
     {
+        // Shop モデルを使って、id に一致するレコードをデータベースから取得
         $shop = Shop::find($id);
+        // Category モデルから全データを取得し、pluckで 'name' を value に、'id' を key にする
         $categories = Category::all()->pluck('name', 'id');
+        // edit ビューに shop と categories を渡す
+        // edit.blade.php に $shop（編集する店舗情報）と $categories（カテゴリ一覧）を渡す。
         return view('edit', ['shop' => $shop, 'categories' => $categories]);
     }
 
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -14,4 +14,10 @@ class Shop extends Model
     public function category(){
         return $this->belongsTo('App\Models\Category');
     }
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    
 }
